f75bf34427f0f1e5c8aa0393ef14217315c1bc56
IanWearsHat <56898254+IanWearsHat@users.noreply.github.com>
2024-02-16 20:09:36 -0800

    LED Fluctuation with OpenSimplex (Untested)

--- src/main/java/frc/robot/subsystems/LEDs/DriveTrainLEDs.java
+++ src/main/java/frc/robot/subsystems/LEDs/DriveTrainLEDs.java
@@ -3,14 +3,20 @@ package frc.robot.subsystems.LEDs;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.LEDs.OpenSimplex;
 
 public class DriveTrainLEDs extends SubsystemBase {
     AddressableLED m_led;
     AddressableLEDBuffer m_ledBuffer;
-    int m_rainbowFirstPixelHue = 0;
 
     static int hue = 80;
     static int value = 255;
+
+    //Variables for noise things
+    static int fluctuationRange = 10;
+    static double fluctuationRate = 0.1;
+    static double offset = 0;
+
 
     /** Creates a new LEDs. */
     public DriveTrainLEDs() {
@@ -46,8 +52,30 @@ public class DriveTrainLEDs extends SubsystemBase {
         return (range[1]-range[0]) * lerpVal + range[0];
     }
 
+    private double Remap(double startVal, double[] startRange, double[] endRange){
+        return (startVal-startRange[0]) * (endRange[1]-endRange[0]) / (startRange[1]- startRange[0]) + endRange[0];
+    }
+
+    private void solidNoiseFluctuation()
+    {
+        double noiseVal = OpenSimplex.noise2_ImproveX(1934173838, 0, offset);
+        noiseVal *= fluctuationRange;
+        noiseVal += hue;
+        noiseVal = (noiseVal + 181) % 181;
+        hue = (int)noiseVal;
+
+        offset += fluctuationRate;
+        if(Math.abs(offset) > 100000 ){ 
+            fluctuationRate *= -1;
+        }
+    }
+
     @Override
     public void periodic() {
+        //Gives the hue color a bit of fluctuation using OpenSimplex noise
+        solidNoiseFluctuation();
+
+        //Set all LEDs to the hue color
         for (var i = 0; i < m_ledBuffer.getLength(); i++) {
             m_ledBuffer.setHSV(i, hue, 255, value);
         }
